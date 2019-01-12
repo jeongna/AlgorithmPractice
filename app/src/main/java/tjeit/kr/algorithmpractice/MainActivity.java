77a54cee55547b443b8ca32cc83878aa5708ae96
Daeki <kimdaeki@gmail.com>
2019-01-12 10:48:33 +0900

    4번 문제 출제 준비

--- app/src/main/java/tjeit/kr/algorithmpractice/MainActivity.java
+++ app/src/main/java/tjeit/kr/algorithmpractice/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends BaseActivity {
     private android.widget.Button question01Btn;
     private Button question02Btn;
     private Button question03Btn;
+    private Button question04Btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +48,13 @@ public class MainActivity extends BaseActivity {
             }
         });
 
+        question04Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, Question04Activity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
@@ -58,6 +66,7 @@ public class MainActivity extends BaseActivity {
     @Override
     public void bindViews() {
 
+        this.question04Btn = (Button) findViewById(R.id.question04Btn);
         this.question03Btn = (Button) findViewById(R.id.question03Btn);
         this.question02Btn = (Button) findViewById(R.id.question02Btn);
         this.question01Btn = (Button) findViewById(R.id.question01Btn);
