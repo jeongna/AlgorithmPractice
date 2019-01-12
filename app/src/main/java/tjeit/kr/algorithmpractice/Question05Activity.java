2ea3d85cb813e76f024b0e9e92bb95d4cfe9f29f
Daeki <kimdaeki@gmail.com>
2019-01-12 12:46:13 +0900

    사용자 입력값이 S/B 카운트 체크

--- app/src/main/java/tjeit/kr/algorithmpractice/Question05Activity.java
+++ app/src/main/java/tjeit/kr/algorithmpractice/Question05Activity.java
@@ -1,9 +1,11 @@
 package tjeit.kr.algorithmpractice;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class Question05Activity extends BaseActivity {
 
@@ -27,6 +29,83 @@ public class Question05Activity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkUserNumber();
+            }
+        });
+
+    }
+
+//    ?S ?B인지 체크하는 메쏘드
+    void checkUserNumber() {
+
+//        세자리가 아닐경우 다시 입력하게 Toast
+
+        if (numInputEdt.length() != 3) {
+            Toast.makeText(mContext, "3자리 숫자를 입력하세요.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+//        사용자가 입력한 숫자를 저장할 세칸짜리 배열.
+//        152 => [1],[5],[2]
+        int[] userInputIntArray = new int[3];
+
+//        사용자가 입력한 값을 String으로 따내자.
+        String inputStr = numInputEdt.getText().toString();
+
+//        배열에 각 자리의 숫자를 집어넣기.
+
+//        먼저, String -> 숫자로 변경. "512" => 512 % 100 / 10 = 12 /10 = 1
+        int inputNumber = Integer.parseInt(inputStr);
+
+//        0번칸 : 맨 앞자리를 대입하는 방법?
+        userInputIntArray[0] = inputNumber / 100;
+
+//        1번칸 : 가운데 자리를 따내는 방법?
+        userInputIntArray[1] = inputNumber % 100 / 10;
+
+//        2번칸 : 맨 뒷자리를 따내는 방법?
+        userInputIntArray[2] = inputNumber % 10;
+
+
+//        숫자를 다 집어넣었으면, 실제로 정답과 비교해서 ?S ?B인지 계산.
+
+        // ?S인지 저장될 변수
+        int strikeCount = 0;
+//        ?B인지 저장될 변수
+        int ballCount = 0;
+
+//        i : 사용자가 입력한 값을 담당하는 index
+        for (int i=0 ; i < 3; i++) {
+
+//            j : 정답지를 돌아보는 index
+            for (int j=0 ; j<3 ; j++) {
+
+//                숫자가 같은걸 발견!
+                if (userInputIntArray[i] == questionIntArray[j]) {
+                    if (i == j) {
+//                        위치도 같다! strike 갯수 증가
+                        strikeCount++;
+                    }
+                    else {
+//                        숫자는 같지만, 위치는 다르다!
+//                        ball 카운트를 증가.
+
+                        ballCount++;
+                    }
+
+                }
+
+            }
+
+        }
+
+//        임시로 토스트에 S/B 출력
+        String temp = String.format("%d S %d B 입니다.", strikeCount, ballCount);
+        Toast.makeText(mContext, temp, Toast.LENGTH_SHORT).show();
 
     }
 
